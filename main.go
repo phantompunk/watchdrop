b833a7167bc69cbf0454409a8e96ef6de752fb8c
Rodrigo Moran <rigo.moran0@gmail.com>
2024-08-14 23:03:40 -0300

    add email alert

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/smtp"
+	"os"
 	"strings"
 	"text/template"
 
@@ -18,6 +20,7 @@ type ProductDetail struct {
 func main() {
 	http.HandleFunc("/", homeFunc)
 	http.HandleFunc("/search", searchFunc)
+	http.HandleFunc("/watchlist", watchFunc)
 
 	fmt.Println("Started http server on port 8080")
 	http.ListenAndServe(":8080", nil)
@@ -54,7 +57,6 @@ func searchFunc(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 	}
 
-	// homeTpl.Execute(w, struct{Name, Price string}{Price: "413", Name:"Nike AirForce1"})
 	homeTpl.Execute(w, productInfo)
 }
 
@@ -65,16 +67,43 @@ func scrapeInfo(url string) (ProductDetail, error) {
 	c := colly.NewCollector()
 
 	c.OnHTML("body", func(el *colly.HTMLElement) {
-    prices := strings.Split(el.ChildText("#price-container > span"), "$")
-    fmt.Println(prices)
-    price = prices[1]
-    name = el.ChildText("#title-container > h1")
-    site = "nike.com"
-    
-    fmt.Println("Site: ", site)
-    fmt.Println("Name: ", name)
-    fmt.Println("Price: ", price)
+		prices := strings.Split(el.ChildText("#price-container > span"), "$")
+		fmt.Println(prices)
+		price = prices[1]
+		name = el.ChildText("#title-container > h1")
+		site = "nike.com"
+
+		fmt.Println("Site: ", site)
+		fmt.Println("Name: ", name)
+		fmt.Println("Price: ", price)
 	})
 	c.Visit(url)
 	return ProductDetail{Name: name, Price: price, Site: site}, nil
+}
+
+const (
+	EMAILER_SYSTEM    = "GMAIL_SENDER_ADDRESS"
+	EMAILER_APP_TOKEN = "GMAIL_SENDER_TOKEN"
+)
+
+func watchFunc(w http.ResponseWriter, r *http.Request) {
+	watcher := r.PostFormValue("email")
+	fmt.Printf("Emailing %s", watcher)
+
+	from := os.Getenv(EMAILER_SYSTEM)
+	password := os.Getenv(EMAILER_APP_TOKEN)
+	if from == "" || password == "" {
+		fmt.Println("Missing system email address or password")
+	}
+	to := []string{watcher}
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	msg := []byte("This is a simple email body.")
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, msg)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
